e1dbc57b45cdac52f37aa6496eaca741f54b1f3c
F0sya <nikitachmyria@gmail.com>
2024-05-23 20:06:44 +0300

    Second practice  exercise is done.

--- cpp_array_part2/main.cpp
+++ cpp_array_part2/main.cpp
@@ -4,45 +4,54 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-	const int FULL_SIZE = 15;
-	int arr1[FULL_SIZE], arr2[FULL_SIZE],arr3[FULL_SIZE];
-	int size;
-	int index1 = 0, index2 = 0;
+	const int FULL_SIZE = 10, HALF_SIZE = 5;
+	int arr1[HALF_SIZE], arr2[HALF_SIZE],arr3[FULL_SIZE];
+	int index = 0;
 
-	cout << "Enter a size of array:";
-	do {
-		cin >> size;
-	} while (size > FULL_SIZE || size <= 0);
-
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < HALF_SIZE; i++) {
 		arr1[i] = rand() % 20 - 10;
+		arr2[i] = rand() % 20 - 10;
 	}
 
 	cout << "First array before changes:" << endl;
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < HALF_SIZE; i++) {
 		cout << arr1[i] << " ";
 	}
+	cout << "\n\nSecond array before changes:" << endl;
 
-	for (int i = 0; i < size; i++) {
-		if (i % 2 == 0) {
-			arr2[index1] = arr1[i];
-			index1++;
-		}
-		else {
-			arr3[index2] = arr1[i];
-			index2++;
-		}
-	}
-
-	cout << "\n\nSecond array after changes:" << endl;
-
-	for (int i = 0; i < index1; i++) {
+	for (int i = 0; i < HALF_SIZE; i++) {
 		cout << arr2[i] << " ";
 	}
-	cout << "\n\nThird array after changes:" << endl;
 
-	for (int i = 0; i < index2; i++) {
+	for (int i = 0; i < HALF_SIZE; i++) {
+		if (arr1[i] > 0) {
+			arr3[index++] = arr1[i];
+		}
+		if (arr2[i] > 0) {
+			arr3[index++] = arr2[i];
+		}
+	}
+	for (int i = 0; i < HALF_SIZE; i++) {
+		if (arr1[i] == 0) {
+			arr3[index++] = arr1[i];
+		}
+		if (arr2[i] == 0) {
+			arr3[index++] = arr2[i];
+		}
+	}
+	for (int i = 0; i < HALF_SIZE; i++) {
+		if (arr1[i] < 0) {
+			arr3[index++] = arr1[i];
+		}
+		if (arr2[i] < 0) {
+			arr3[index++] = arr2[i];
+		}
+	}
+
+	cout << "\n\nResult array after changes:" << endl;
+
+	for (int i = 0; i < FULL_SIZE; i++) {
 		cout << arr3[i] << " ";
 	}
 }
