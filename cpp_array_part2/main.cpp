b3245c262c04fa9010a42efce872017c1648b843
F0sya <nikitachmyria@gmail.com>
2024-05-23 19:01:15 +0300

    First practice exercise is done

--- cpp_array_part2/main.cpp
+++ cpp_array_part2/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
+	srand(time(0));
+	const int FULL_SIZE = 15;
+	int arr[FULL_SIZE];
+	int size;
+	int count = 0;
 
+	cout << "Enter a size of array:";
+	do {
+		cin >> size;
+	} while (size > FULL_SIZE || size <= 0);
+	
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 20 - 10;
+	}
+
+	cout << "Array before changes:" << endl;
+
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+
+	for (int i = 0; i < size; i++) {
+		if (arr[i] != 0) {
+			arr[count++] = arr[i];
+		}
+	}
+
+	cout << endl << endl << "Array afther changes:" << endl;
+
+	for (int i = 0; i < size; i++) {
+		if (i >= count) {
+			arr[i] = -1;
+		}
+	}
+
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
 }
